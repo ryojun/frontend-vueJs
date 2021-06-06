744e7a68f656431442e096ce48ac9df4f67b4b8c
ryojun <ryojun29378@gmail.com>
2021-06-06 22:06:09 +0700

    start vueJs project

---
+++ src/views/Register.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="register container">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <img src="/img/logo.svg" alt="Logo" class="img-logo">
+                <div class="card">
+                    <h1 class="card-header">
+                        REGISTER
+                    </h1>
+                    <div class="card-body">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.img-logo {
+    width: 80%;
+    display: block;
+    margin: auto;
+    margin-top: 15%;
+    margin-bottom: 30px;
+}
+h1 {
+  font-size: 18px;
+  font-weight: 600;
+  text-align: center;
+  letter-spacing: 5px;
+}
+</style>
+
+
